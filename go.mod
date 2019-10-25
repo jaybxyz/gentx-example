76ebba34bdf97decdd4b9f68bafa1d575ec4a17c
kogisin <jaybkim2@gmail.com>
2019-10-25 18:38:13 +0900

    Update dependencies versions

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.12
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/cosmos/cosmos-sdk v0.34.9
+	github.com/cosmos/cosmos-sdk v0.34.4-0.20191010193331-18de630d0ae1
 	github.com/pkg/errors v0.8.1
-	github.com/tendermint/tendermint v0.31.10
+	github.com/tendermint/tendermint v0.32.6
 	gopkg.in/resty.v1 v1.12.0
 )
--- main_test.go
+++ main_test.go
@@ -11,7 +11,8 @@ import (
 )
 
 func TestVerify(t *testing.T) {
-	var resp = `{"height":"2274483","txhash":"CF5C0328C1243E50A4AAA536288FA2BD2DC9FD08837FFAF8E0AB3070B09D12A1","raw_log":"[{\"msg_index\":\"0\",\"success\":true,\"log\":\"\"}]","logs":[{"msg_index":"0","success":true,"log":""}],"gas_wanted":"200000","gas_used":"103821","tags":[{"key":"action","value":"create_validator"},{"key":"destination-validator","value":"cosmosvaloper1khnuwdpnyv6utvqx3plt3x6makh3rc9tv0wvv9"},{"key":"moniker","value":"cosmos-sta"},{"key":"identity"}],"tx":{"type":"auth/StdTx","value":{"msg":[{"type":"cosmos-sdk/MsgCreateValidator","value":{"description":{"moniker":"cosmos-sta","identity":"","website":"","details":""},"commission":{"rate":"0.100000000000000000","max_rate":"0.200000000000000000","max_change_rate":"0.010000000000000000"},"min_self_delegation":"1","delegator_address":"cosmos1khnuwdpnyv6utvqx3plt3x6makh3rc9tfm6eqk","validator_address":"cosmosvaloper1khnuwdpnyv6utvqx3plt3x6makh3rc9tv0wvv9","pubkey":"cosmosvalconspub1zcjduepqgygxh0pntlyhqhrdvzwchezzs0wut0ug7aa4dfym8xcyqce7qw8spflzc3","value":{"denom":"uatom","amount":"1000000"}}}],"fee":{"amount":[{"denom":"uatom","amount":"5000"}],"gas":"200000"},"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"A5sGJWDI/DfZS5MNoaxGEgWEb0CARrWGPPj4KR/fNhTv"},"signature":"CPbUTKQ3LqWjx4Smmjy0IzTGYnuP5rgfBXfWdfezeVE79ZGcJSmDEGhotcrXN1Rgvo5mP+8y1nV+bRpJXMuMCA=="}],"memo":""}},"timestamp":"2019-10-22T03:30:17Z"}`
+	// failed gentx
+	var resp = `{"type":"cosmos-sdk/StdTx","value":{"msg":[{"type":"cosmos-sdk/MsgCreateValidator","value":{"description":{"moniker":"Cosmostation","identity":"","website":"","details":""},"commission":{"rate":"0.100000000000000000","max_rate":"0.200000000000000000","max_change_rate":"0.010000000000000000"},"min_self_delegation":"1","delegator_address":"kava1qrlge6kqjz2763yp6ghws9ekv8u62dva9hs86p","validator_address":"kavavaloper1qrlge6kqjz2763yp6ghws9ekv8u62dvagp20zk","pubkey":"kavavalconspub1zcjduepqnlcxz09tphlp2skvmkyxt6rq0jhfhnwy6thq3v2j4t8cj4pvnn3sp76zg8","value":{"denom":"ukava","amount":"50000000"}}}],"fee":{"amount":[],"gas":"200000"},"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"AxhDEq4xW6Yfsl/VWN+ASRJV5mf2MGFyhj8sewIhE9Hj"},"signature":"I02IWE6EdHw377OD6YefRlY3/FTjF4ePQGHKZmYy6n5ToD9M8ZTso/ceq3y4oTYii66EvUC1Pgm74gMGNomhxw=="}],"memo":"cd2705b287f8a28b4bdb6f37dc23511cd76ce2fe@182.77.0.5:26656"}}`
 
 	var tx sdk.TxResponse
 
@@ -25,30 +26,30 @@ func TestVerify(t *testing.T) {
 
 	cdc.MustUnmarshalJSON([]byte(resp), &tx)
 
-	// fmt.Println(tx)
-	// fmt.Println("")
+	fmt.Println(tx)
+	fmt.Println("")
 
-	stdTx := tx.Tx.(*auth.StdTx)
+	// stdTx := tx.Tx.(*auth.StdTx)
 
-	// stdSigs contains the sequence number, account number, and signatures.
-	stdSigs := stdTx.GetSignatures()
-	for i := 0; i < len(stdSigs); i++ {
-		// TODO: Get signer account from somewhere you trust
-		// ----------- e.g. ------------
-		signer, _ := sdk.AccAddressFromBech32("cosmos1khnuwdpnyv6utvqx3plt3x6makh4ee1tfm6eqe")
-		acc := auth.BaseAccount{
-			Address:       signer,
-			AccountNumber: 21634, // from lcd
-			Sequence:      1,     //from lcd
-			PubKey:        stdSigs[i].PubKey,
-		}
-		// ----------- e.g. ------------
+	// // stdSigs contains the sequence number, account number, and signatures.
+	// stdSigs := stdTx.GetSignatures()
+	// for i := 0; i < len(stdSigs); i++ {
+	// 	// TODO: Get signer account from somewhere you trust
+	// 	// ----------- e.g. ------------
+	// 	signer, _ := sdk.AccAddressFromBech32("cosmos1khnuwdpnyv6utvqx3plt3x6makh4ee1tfm6eqe")
+	// 	acc := auth.BaseAccount{
+	// 		Address:       signer,
+	// 		AccountNumber: 21634, // from lcd
+	// 		Sequence:      1,     //from lcd
+	// 		PubKey:        stdSigs[i].PubKey,
+	// 	}
+	// 	// ----------- e.g. ------------
 
-		signBytes := auth.GetSignBytes("cosmoshub-2", *stdTx, &acc, false)
+	// 	signBytes := auth.GetSignBytes("cosmoshub-2", *stdTx, &acc, false)
 
-		fmt.Println(string(signBytes))
+	// 	fmt.Println(string(signBytes))
 
-		pubKey := stdSigs[i].PubKey
-		fmt.Printf("Verified: %t", pubKey.VerifyBytes(signBytes, stdSigs[i].Signature))
-	}
+	// 	pubKey := stdSigs[i].PubKey
+	// 	fmt.Printf("Verified: %t", pubKey.VerifyBytes(signBytes, stdSigs[i].Signature))
+	// }
 }
